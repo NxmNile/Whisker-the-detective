378d0c8ab70831704003ee4197d534165eb7b0c9
NxmNile <laksikapanich@gmail.com>
2024-06-18 11:24:58 +0900

    Owl house
    Add movement/ collider to scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    private float initialSpeed;
+    private Animator animator;
+    private Rigidbody rb;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        initialSpeed = moveSpeed;
+    }
+    
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical= Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical) * moveSpeed * Time.deltaTime;
+        
+        rb.MovePosition(transform.position + movement);
+
+        // Rotate the player model based on movement direction
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
+        }
+
+        // Update animator parameter
+        bool isMoving = moveHorizontal != 0 || moveVertical != 0;
+        animator.SetBool("IsWalk", isMoving);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        string objectName = other.gameObject.name;
+        if (objectName == "Hootie"||objectName=="oven"||objectName=="desk"||objectName=="chair"||objectName=="WARDOBE")
+        {
+            moveSpeed = 0;
+        }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        moveSpeed = initialSpeed;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        moveSpeed = initialSpeed;
+    }
+}
