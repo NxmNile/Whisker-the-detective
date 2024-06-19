a8c54ae2b361879916e302dae6d36f6c10e0dd10
NxmNile <laksikapanich@gmail.com>
2024-06-19 11:47:07 +0900

    Switch Camera Script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Camera RabbitCamera;
+    [SerializeField] private Camera OwlCamera;
+    [SerializeField] private Camera SquirrelCamera;
+    [SerializeField] private Camera BadgerCamera;
+    [SerializeField] private Camera PoliceCamera;
+    private Camera[] allcamera;
+    private void Start()
+    {
+        MainCamera.enabled = true;
+        allcamera = new[] { MainCamera, RabbitCamera, OwlCamera, SquirrelCamera, BadgerCamera, PoliceCamera };
+        ChangeCamera(2);
+    }
+
+    public void ChangeCamera(int cameraNumber)
+    {
+        for (int j = 0; j < allcamera.Length; j++)
+        {
+            if(j==cameraNumber)
+            {
+                allcamera[j].enabled = true;
+            }
+            else
+            {
+                allcamera[j].enabled = false;
+            }
+        }
+    }
+    
+}
