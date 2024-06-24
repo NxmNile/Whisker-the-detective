1a1492ba474d5ec33ab674b2feba607a20499040
NxmNile <laksikapanich@gmail.com>
2024-06-24 12:12:29 +0900

    fix bug

--- Assets/Scripts/CluesInvestigate.cs
+++ Assets/Scripts/CluesInvestigate.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.SceneManagement;
-using UnityEditorInternal;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
